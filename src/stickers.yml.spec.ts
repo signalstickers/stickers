71e83050a9d376e9300c30bc77ee0b1babd279ae
darkobits <joshua+github@frontlawn.net>
2021-01-29 04:00:42 -0800

    tests: Guard against accidental mass deletion of sticker packs.

--- src/stickers.yml.spec.ts
+++ src/stickers.yml.spec.ts
@@ -1,9 +1,17 @@
+import axios from 'axios';
+
 import {
   parseErrors,
   parseYaml,
   validate
 } from 'lib/test-utils';
-// import log from 'lib/log';
+
+
+/**
+ * The maximum number of sticker packs a commit/PR is allowed to remove from the
+ * directory.
+ */
+const MAX_ALLOWED_DELETIONS = 5;
 
 
 describe('stickers.yml', () => {
@@ -20,6 +28,18 @@ describe('stickers.yml', () => {
       }
     } catch (err) {
       fail(err);
+    }
+  });
+
+  it(`should not decrease the number of sticker packs by more than ${MAX_ALLOWED_DELETIONS}`, async () => {
+    const existingPartials = (await axios.get('https://signalstickers.github.io/stickers/partials.json')).data;
+    const numExistingPacks = existingPartials.length;
+    // @ts-expect-error
+    const numLocalPacks = Object.keys(parseYaml(require.resolve('./stickers.yml'))).length;
+    const packsDiff = numLocalPacks - numExistingPacks;
+
+    if (packsDiff < -MAX_ALLOWED_DELETIONS) {
+      fail(`This commit would delete ${Math.abs(packsDiff)} sticker packs, which is  more than the allowed maximum of ${MAX_ALLOWED_DELETIONS}.`);
     }
   });
 });
